690ea3787ed4b0c374d253bc1da15259192aecea
Rez <rezhates@gmail.com>
2025-10-18 00:26:39 +0530

    Stretching the cursor

--- src/components/StickyCursor.jsx
+++ src/components/StickyCursor.jsx
@@ -7,7 +7,7 @@ const StickyCursor = ({ stickyElement }) => {
   const mouse = useRef({ x: 0, y: 0 });
   const circle = useRef(null);
   const animationId = useRef(null);
- 
+  const scale = useRef({ x: 1, y: 1 });
 
   const manageMouseMove = (e) => {
     const { clientX, clientY } = e;
@@ -16,10 +16,12 @@ const StickyCursor = ({ stickyElement }) => {
       y: clientY,
     };
   };
-  const moveCircle = (x, y) => {
+  const moveCircle = (x, y,scaleX = 1, scaleY = 1) => {
     gsap.to(circle.current, {
       x,
       y,
+      scaleX, // Add scaleX
+      scaleY,
       xPercent: -50,
       yPercent: -50,
       delay: 0.02,
@@ -32,21 +34,27 @@ const StickyCursor = ({ stickyElement }) => {
 
       const centerX = left + width / 2;
       const centerY = top + height / 2;
-      const targetX = centerX + (mouse.current.x - centerX) * 0.1;
-      const targetY = centerY + (mouse.current.y - centerY) * 0.1;
+      const distX = (mouse.current.x - centerX);
+      const distY = (mouse.current.y - centerY) ;
+      const targetX = centerX + distX * 0.1;
+      const targetY = centerY + distY* 0.1;
+      const absDistance = Math.max(Math.abs(distX), Math.abs(distY));
+      const newScaleX = gsap.utils.mapRange(0, width / 2, 1, 1.3, absDistance);
+      const newScaleY = gsap.utils.mapRange(0, height / 2, 1, 0.8, absDistance);
+      scale.current.x = newScaleX;
+      scale.current.y = newScaleY;
 
-      moveCircle(targetX, targetY);
+      moveCircle(targetX, targetY,newScaleX, newScaleY);
     } else {
-      moveCircle(mouse.current.x, mouse.current.y);
+      moveCircle(mouse.current.x, mouse.current.y,1,1);
     }
-   animationId.current = window.requestAnimationFrame(animate);
+    animationId.current = window.requestAnimationFrame(animate);
   };
   const manageMouseOver = () => {
     setIsHovered(true);
   };
   const manageMouseLeave = () => {
     setIsHovered(false);
-    
   };
   useEffect(() => {
     const element = stickyElement.current;
@@ -61,7 +69,6 @@ const StickyCursor = ({ stickyElement }) => {
       element.removeEventListener("mouseover", manageMouseOver);
       element.removeEventListener("mouseleave", manageMouseLeave);
       if (animationId.current) {
-
         cancelAnimationFrame(animationId.current);
       }
     };
@@ -72,6 +79,7 @@ const StickyCursor = ({ stickyElement }) => {
       className="fixed bg-black rounded-full pointer-events-none"
       style={{
         width: size,
+        
         height: size,
         transition: "height 0.3s ease-out , width 0.3s ease-out",
       }}
