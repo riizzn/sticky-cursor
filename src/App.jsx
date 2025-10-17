0ae84634b79c8abdfb144273e52e339d14a32eff
Rez <rezhates@gmail.com>
2025-10-17 11:16:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+import StickyCursor from './components/StickyCursor'
+
+const App = () => {
+  return (
+    <main>
+      <Header/>
+      <StickyCursor/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="burger"></div>
+    </div>
+  );
+};
+
+export default Header;
